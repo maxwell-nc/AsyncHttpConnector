1624d242bc7b5b84f7a437dc8ebeb4aea28e6496
Maxwell-nc <maxwell-nc@outlook.com>
2016-05-18 17:01:27 +0800

    更新版本及更新demo

--- app/src/main/java/pres/nc/maxwell/asynchttpconnector/MainActivity.java
+++ app/src/main/java/pres/nc/maxwell/asynchttpconnector/MainActivity.java
@@ -1,26 +1,75 @@
 package pres.nc.maxwell.asynchttpconnector;
 
 import android.os.Bundle;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
+
+import java.io.File;
 
 import pres.nc.maxwell.asynchttp.HttpConnector;
+import pres.nc.maxwell.asynchttp.callback.impl.FileCallback;
 import pres.nc.maxwell.asynchttp.callback.impl.StringCallback;
 import pres.nc.maxwell.asynchttp.response.Response;
 
 public class MainActivity extends AppCompatActivity {
 
+    String url = "https://www.baidu.com";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        init();
-        getSample();
     }
 
-    public void init() {
+    public void get(View view) {
 
-        //HttpConnector.clearCache(this);//清除所有缓存
+        HttpConnector.get(url)
+                .addParams("params", "value")//参数
+                .log("tag")//打印日志的标记，不设置则不打印
+                //.cache()//使用缓存,使用这个方法必须配置过全局属性
+                //.cache(3600000L)//使用缓存,使用这个方法必须配置过全局属性,独立的缓存时间
+                .cache(this, 3600000L)//使用缓存,独立的缓存时间
+                //.setConnectTimeout(10000)//独立的连接超时时间
+                //.setReadTimeout(15000)//独立的读取超时时间
+                .callback(new StringCallback() {
+
+                    @Override
+                    public void onAfter(Response<String> response) {
+                        Log.i("Response Data:", response.getResponseData());
+                    }
+
+                })
+                .load();
+
+    }
+
+    public void download(View view) {
+
+        HttpConnector.get("http://pic32.nipic.com/20130829/12906030_124355855000_2.png")
+                .log("file")
+                .setConnectTimeout(20000)
+                .setReadTimeout(30000)
+                .callback(new FileCallback(Environment.getExternalStorageDirectory().getPath() + "/test.png") {
+
+                    @Override
+                    public void onSuccess(Response<File> response) {
+                        Toast.makeText(MainActivity.this, "success", Toast.LENGTH_SHORT).show();
+                    }
+
+                    @Override
+                    public void onDownProgress(float progress, int fileSize) {
+                        Log.e("onDownProgress", "当前进度：" + progress * 100 + "% 总大小为：" + fileSize);
+                    }
+
+                })
+                .load();
+
+    }
+
+    public void set(View view) {
 
         HttpConnector.globalConfig(this)
                 .setDefaultConnectTimeout(10000)//默认连接超时时间（毫秒），这里是10秒
@@ -31,31 +80,17 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    public void getSample() {
-
-        String url = "https://www.baidu.com";
-
-        //HttpConnector.clearCache(this,url);//清除指定缓存
-
-        HttpConnector.get(url)
-                .addParams("params", "value")//参数
-                .log("tag")//打印日志的标记，不设置则不打印
-                .cache()//使用缓存,使用这个方法必须配置过全局属性
-                //.cache(3600000L)//使用缓存,使用这个方法必须配置过全局属性,独立的缓存时间
-                //.cache(this,3600000L)//使用缓存,独立的缓存时间
-                //.setConnectTimeout(10000)//独立的连接超时时间
-                //.setReadTimeout(15000)//独立的读取超时时间
-                .callback(new StringCallback() {
-
-                    @Override
-                    public void onSuccess(Response<String> response) {
-                        Log.i("Response Data:", response.getResponseData());
-                    }
-
-                })
-                .load();
-
+    public void clearSome(View view) {
+        HttpConnector.clearCache(this, url);//清除指定缓存
     }
 
+    public void clearAll(View view) {
+        HttpConnector.clearCache(this);//清除所有缓存
+    }
+
+    public void cancel(View view) {
+        //待实现
+        Toast.makeText(MainActivity.this, "待实现", Toast.LENGTH_SHORT).show();
+    }
 }
 
